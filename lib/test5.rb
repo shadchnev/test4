cd965ad0960b010cea9c1e7682c5211da3e44d51
Simon Woolf <simon@simonwoolf.net>
2013-12-06 10:07:53 +0000

    iterative & recursive

--- lib/test5.rb
+++ lib/test5.rb
@@ -1,5 +1,5 @@
 class Array
-  def my_inject(*params, &block)
+  def recursive_inject(*params, &block)
     if !block_given?
       # params is either [initial, op] or [op]
       operation = params.pop
@@ -12,11 +12,23 @@ class Array
     self[1..-1].inject(new_initial, &block) 
   end
 
-  def my_iterative_inject(operation)
-    result = self[0]
-    self[1..-1].each do |element| 
-      result = result.send(operation, element)
+  def iterative_inject(*params, &block)
+    if !block_given?
+      # params is either [initial, op] or [op]
+      operation = params.pop
+      block = lambda {|acc, e| acc.send(operation, e)} 
     end
-    result
+    # params is either [initial] or []
+    initial = params.pop
+    return initial if self.empty?
+    accumulator =  initial.nil? ? self[0] : block.call(initial, self[0])
+    self[1..-1].each do |element| 
+      accumulator = block.call(accumulator, element)
+    end
+    accumulator
   end
+
+
+
+
 end
--- spec/test5_spec.rb
+++ spec/test5_spec.rb
@@ -1,26 +1,31 @@
 require 'test5'
 
 describe Array do
-  let(:array) {MyArray[1,2,3,4,5]}
+  let(:array) {Array[1,2,3,4,5]}
 
   it 'should sum with a block' do
-    expect(array.my_inject{|acc, e| acc += e}).to eq 15
+    expect(array.recursive_inject{|acc, e| acc += e}).to eq 15
+    expect(array.iterative_inject{|acc, e| acc += e}).to eq 15
   end
 
   it 'should sum without a block' do
-    expect(array.my_inject(:+)).to eq 15
+    expect(array.recursive_inject(:+)).to eq 15
+    expect(array.iterative_inject(:+)).to eq 15
   end
 
   it 'should product' do
-    expect(array.my_inject(:*)).to eq 120 
+    expect(array.recursive_inject(:*)).to eq 120 
+    expect(array.iterative_inject(:*)).to eq 120 
   end
 
   it 'should take an initial value' do
-    expect(array.my_inject(-15){|acc, e| acc += e}).to eq 0
+    expect(array.recursive_inject(-15){|acc, e| acc += e}).to eq 0
+    expect(array.iterative_inject(-15){|acc, e| acc += e}).to eq 0
   end
 
   it 'should take both an initial value and an operation' do
-    expect(array.my_inject(-15, :+)).to eq 0
+    expect(array.recursive_inject(-15, :+)).to eq 0
+    expect(array.iterative_inject(-15, :+)).to eq 0
   end
 end
 
