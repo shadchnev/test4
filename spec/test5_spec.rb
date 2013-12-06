cdabde127e2469faa759f993aa4e8c38e1324407
Simon Woolf <simon@simonwoolf.net>
2013-12-06 09:47:44 +0000

    clean up unnecessary

--- spec/test5_spec.rb
+++ spec/test5_spec.rb
@@ -1,21 +1,26 @@
 require 'test5'
 
-describe 'Enumerable.my_inject' do
-  it 'should sum' do
-    [1,2,3,4].my_inject(:+).should == 10
+describe Array do
+  let(:array) {MyArray[1,2,3,4,5]}
+
+  it 'should sum with a block' do
+    expect(array.my_inject{|acc, e| acc += e}).to eq 15
   end
 
-  it 'should do the same thing as the previous it block' do
-    [1,2,3,4,5].my_inject(:+).should == 15
-  end
-
-  it 'should not sum' do
-    [1,2,3,4,5].my_inject(:+).should_not == 10
+  it 'should sum without a block' do
+    expect(array.my_inject(:+)).to eq 15
   end
 
   it 'should product' do
-    [1,2,3,4].my_inject(:*).should == 24 
+    expect(array.my_inject(:*)).to eq 120 
   end
 
-  it 'should '
+  it 'should take an initial value' do
+    expect(array.my_inject(-15){|acc, e| acc += e}).to eq 0
+  end
+
+  it 'should take both an initial value and an operation' do
+    expect(array.my_inject(-15, :+)).to eq 0
+  end
 end
+
